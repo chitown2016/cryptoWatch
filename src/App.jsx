886f97be89b5bd0c5be25dc52fe56a768374cfba
Michael Tulum <kocatulum@yahoo.com>
2025-07-18 03:20:30 -0500

    remove count

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [apiData, setApiData] = useState(null)
   const [minMarketCap, setMinMarketCap] = useState('')
 
